7d401eb3dab91b91d0978b12d5fc21442ff82c11
Ayush Lal Shrestha <ayush.shrestha@logpoint.com>
2018-04-30 09:53:04 +0545

    README.md

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -25,10 +25,8 @@ from rest_framework_jwt.views import obtain_jwt_token
 from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
-    
     url(r'^admin/', admin.site.urls),
     url(r'^comments/', include("comments.urls", namespace='comments')),
-    
     url(r'^register/', register_view, name='register'),
     url(r'^login/', login_view, name='login'),
     url(r'^logout/', logout_view, name='logout'),
