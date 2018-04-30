0a27d191bad2fda4986ff644017e4e0f934ca7d0
Ayush Lal Shrestha <ayush.shrestha@logpoint.com>
2018-04-30 09:18:08 +0545

    DRF for blog api

---
+++ virtualenv-environment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/logpoint/ayush_files/my_projects/python_projects/Blog-API-with-Django-Rest-Framework/Blog-api-environment/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
